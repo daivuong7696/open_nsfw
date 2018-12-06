84bc16dc61c630b5a7248d77d4c6a3c0b17cf93a
Dai Vuong <daivuong7696@gmail.com>
2018-12-06 15:12:04 +0700

    Save cam for many images

--- classify_nsfw.py
+++ classify_nsfw.py
@@ -25,8 +25,24 @@ except ImportError:
     from io import BytesIO as StringIO
     
 import caffe
-#import visualize_result 
-from sklearn.metrics import classification_report, confusion_matrix, accuracy_score, roc_curve    
+import cv2
+from class_activation_map import save_CAM_caffe
+
+try:
+    caffe_root = os.environ['CAFFE_ROOT'] + '/'
+except KeyError:
+    raise KeyError("Define CAFFE_ROOT in ~/.bashrc")
+
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score, roc_curve
+
+
+class_dict = {
+    'notsexy': 0,
+    'sexy': 1
+}
+
+
+
 def resize_image(data, sz=(256, 256)):
     """
     Resize image. Please use this resize logic for best results instead of the 
@@ -49,11 +65,29 @@ def resize_image(data, sz=(256, 256)):
     fh_im.seek(0)
     return bytearray(fh_im.read())
 
-def caffe_preprocess_and_compute(pimg, caffe_transformer=None, caffe_net=None,
-    output_layers=None):
+def caffe_preprocess(caffe_net, image_data,
+                     caffe_transformer=None):
+
+    img_data_rs = resize_image(image_data, sz=(256, 256))
+    image = caffe.io.load_image(StringIO(img_data_rs))
+
+    H, W, _ = image.shape
+    _, _, h, w = caffe_net.blobs['data'].data.shape
+    h_off = int(max((H - h) / 2, 0))
+    w_off = int(max((W - w) / 2, 0))
+    crop = image[h_off:h_off + h, w_off:w_off + w, :]
+    transformed_image = caffe_transformer.preprocess('data', crop)
+    transformed_image.shape = (1,) + transformed_image.shape
+
+    return image, transformed_image
+
+
+def caffe_compute(transformed_image,
+                  caffe_net=None, output_layers=None):
     """
     Run a Caffe network on an input image after preprocessing it to prepare
     it for Caffe.
+
     :param PIL.Image pimg:
         PIL image to be input into Caffe.
     :param caffe.Net caffe_net:
@@ -65,28 +99,20 @@ def caffe_preprocess_and_compute(pimg, caffe_transformer=None, caffe_net=None,
     :return:
         Returns the requested outputs from the Caffe net.
     """
+
     if caffe_net is not None:
 
         # Grab the default output names if none were requested specifically.
         if output_layers is None:
             output_layers = caffe_net.outputs
 
-        img_data_rs = resize_image(pimg, sz=(256, 256))
-        image = caffe.io.load_image(StringIO(img_data_rs))
-
-        H, W, _ = image.shape
-        _, _, h, w = caffe_net.blobs['data'].data.shape
-        h_off = int(max((H - h) / 2, 0))
-        w_off = int(max((W - w) / 2, 0))
-        crop = image[h_off:h_off + h, w_off:w_off + w, :]
-        transformed_image = caffe_transformer.preprocess('data', crop)
-        transformed_image.shape = (1,) + transformed_image.shape
-
         input_name = caffe_net.inputs[0]
         all_outputs = caffe_net.forward_all(blobs=output_layers,
                     **{input_name: transformed_image})
 
+
         outputs = all_outputs[output_layers[0]][0].astype(float)
+
         return outputs
     else:
         return []
@@ -120,9 +146,14 @@ def main(argv):
         type=float,
         help="Path to the input image file"
     )
+    parser.add_argument(
+        "--save_cam",
+        default=False,
+        type=bool,
+        help="Save class activation map flag"
+    )
 
     args = parser.parse_args()
-    
 
     # Pre-load caffe model.
     nsfw_net = caffe.Net(args.model_def,  # pylint: disable=invalid-name
@@ -131,57 +162,99 @@ def main(argv):
     # Load transformer
     # Note that the parameters are hard-coded for best results
     caffe_transformer = caffe.io.Transformer({'data': nsfw_net.blobs['data'].data.shape})
-    caffe_transformer.set_transpose('data', (2, 0, 1))  # move image channels to outermost
-    caffe_transformer.set_mean('data', np.array([104, 117, 123]))  # subtract the dataset-mean value in each channel
-    caffe_transformer.set_raw_scale('data', 255)  # rescale from [0, 1] to [0, 255]
-    caffe_transformer.set_channel_swap('data', (2, 1, 0))  # swap channels from RGB to BGR
+    # move image channels to outermost
+    caffe_transformer.set_transpose('data', (2, 0, 1))
+    # subtract the dataset-mean value in each channel
+    caffe_transformer.set_mean('data', np.array([104, 117, 123]))
+    # rescale from [0, 1] to [0, 255]
+    caffe_transformer.set_raw_scale('data', 255)
+    # swap channels from RGB to BGR
+    caffe_transformer.set_channel_swap('data', (2, 1, 0))
 
+    # Preprocess and compute image
+    # One image only
     if args.input_file is not None:
         with open(args.input_file, 'rb') as f:
             image_data = f.read()
-        # Classify.
-            scores = caffe_preprocess_and_compute(image_data, caffe_transformer=caffe_transformer, caffe_net=nsfw_net, output_layers=['prob'])
-        print("NSFW score:  " , scores)
+            # Preprocessing
+            original_image, transformed_image = caffe_preprocess(
+                caffe_net=nsfw_net, image_data=image_data,
+                caffe_transformer=caffe_transformer
+            )
+            # Calculating scores
+            scores = caffe_compute(
+                transformed_image=transformed_image, caffe_net=nsfw_net,
+                output_layers=['prob']
+            )
+            # Calculating class activation map
+            if args.save_cam:
+                out_layer = 'fc_nsfw'
+                last_conv = 'conv_stage3_block2_branch2c'
+                weights_LR = nsfw_net.params[out_layer][0].data
+                activation_lastconv = nsfw_net.blobs[last_conv].data
 
-    elif args.input_label_file is not None: 
+                save_CAM_caffe(image_name=args.input_file,
+                    image=original_image, fc_weights=weights_LR,
+                    activation_lastconv=activation_lastconv,
+                    class_dict=class_dict, class_name='sexy',
+                    dest_folder='/home/daivuong/Desktop',
+                    image_size=256
+                )
+        print("NSFW score: {}".format(scores[1]))
+    # Input is a file of many images
+    elif args.input_label_file is not None:
         scores = []
-        df = pd.read_csv(args.input_label_file, header=None, delimiter=' ', names=['file_name', 'label'])
+        df = pd.read_csv(
+            args.input_label_file,
+            header=None, delimiter=' ',
+            names=['file_name', 'label']
+        )
         for i in tqdm(range(len(df))):
             with open(df.iloc[i, 0], 'rb') as f:
-                image = f.read()
-                scores.append(caffe_preprocess_and_compute(image, 
-                                                           caffe_transformer=caffe_transformer, 
-                                                           caffe_net=nsfw_net, 
-                                                           output_layers=['prob'])[1])
+                image_data = f.read()
+                # Preprocessing
+                original_image, transformed_image = caffe_preprocess(
+                    caffe_net=nsfw_net, image_data=image_data,
+                    caffe_transformer=caffe_transformer
+                )
+                # Calculating scores
+                sexy_score = caffe_compute(
+                    transformed_image=transformed_image, caffe_net=nsfw_net,
+                    output_layers=['prob']
+                )[1]
+                scores.append(sexy_score)
+
+                # Caclulating class activation map
+                if args.save_cam:
+                    dest = os.path.join(
+                        '/home/daivuong/Desktop/test', str(round(sexy_score, 1))
+                    )
+                    if not os.path.isdir(dest):
+                        os.mkdir(dest)
+                    out_layer = 'fc_nsfw'
+                    last_conv = 'conv_stage3_block2_branch2c'
+
+                    weights_LR = nsfw_net.params[out_layer][0].data
+                    activation_lastconv = nsfw_net.blobs[last_conv].data
+
+                    save_CAM_caffe(image_name=df.iloc[i, 0],
+                        image=original_image, fc_weights=weights_LR,
+                        activation_lastconv=activation_lastconv,
+                        class_dict=class_dict, class_name='sexy',
+                        dest_folder=dest,
+                        image_size=256
+                    )
         df['scores'] = scores
-        df['NSFW'] = df['scores'] >= args.threshold
+        df['NSFW'] = (df['scores'] >= args.threshold)
         # From boolean to int
         df['NSFW'] = df['NSFW'] + 0
-        
+
         target_names = ['nosexy', 'sexy']
         cnf_matrix = confusion_matrix(df['label'], df['NSFW'])
         y = df['label']
         y_pred = df['NSFW']
         accuracy = accuracy_score(y, y_pred)
         print("Accuracy: {}".format(accuracy))
-        report = classification_report(y, y_pred, target_names=target_names)
-        fpr, tpr, thresholds = roc_curve(y, df['scores'], pos_label=1)
-        print("False positive rate: ", fpr)
-        print("True positive rate: ", tpr)
-        print('thresholds: ', thresholds)
-        
-        plt.figure(1)
-        plt.plot([0, 1], [0, 1], 'k--')
-        plt.plot(fpr, tpr, label='Original caffemodel')
-        plt.xlabel('False positive rate')
-        plt.ylabel('True positive rate')
-        plt.title('ROC curve')
-        plt.legend(loc='best')
-        plt.savefig('test.png')
-#         visualize_result.save_confusion_matrix_classification_report(cnf_matrix=cnf_matrix, 
-#                                                                      classification_report=report,
-#                                                                      class_names=target_names,
-#                                                                      file_name='cnf_matrix')
 
         df[['file_name', 'label', 'scores', 'NSFW']].to_csv(
             'result.txt', sep=' ', header=None, index=None)
